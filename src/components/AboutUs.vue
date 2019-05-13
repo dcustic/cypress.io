d24dcc98b99a872ab84f584cb597a4086991e41d
Dindimon <dino.custic@outletcity.com>
2019-05-13 11:00:05 +0200

    Test szenarien

--- src/components/AboutUs.vue
+++ src/components/AboutUs.vue
@@ -1,9 +1,9 @@
 <template>
-	<div>This is the about us page</div>
+	<div>
+		<div>This is the about us page</div>
+		<img src="https://i.imgur.com/1rrrFbH.jpg" alt="noalt"></div>
 </template>
 
 <script>
-	export default {
-
-	}
+	export default {}
 </script>
